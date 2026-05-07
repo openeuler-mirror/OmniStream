cf10698e71fb5d02f59823e13896b7754e9c4198
zhangmingbai <zmb_work@163.com>
2026-05-07 19:14:24 +0800

    fix flink sp canonical - omni

--- cpp/runtime/state/restore/KeyGroupEntryIterator.h
+++ cpp/runtime/state/restore/KeyGroupEntryIterator.h
@@ -37,7 +37,7 @@ public:
         if (currentIndex_ < entries_.size()) {
             return true;
         }
-        if ((0 != offset_) && (END_OF_KEY_GROUP_MARK & currentKvStateId_ != END_OF_KEY_GROUP_MARK)) {
+        if ((offset_ != 0) && ((END_OF_KEY_GROUP_MARK & currentKvStateId_) != END_OF_KEY_GROUP_MARK)) {
             if (currentIndex_ >= entries_.size()) {
                 // jni read
                 currentIndex_ = 0;
