8dc504fe4701ba8c71773ca342be8533b18fefd6
zhangjjiabo <57910833@qq.com>
2026-05-22 09:27:26 +0800

    Fix undefined reference by moving createOutputBatch template implementation to header

--- cpp/table/runtime/operators/window/AggregateWindowOperator.h
+++ cpp/table/runtime/operators/window/AggregateWindowOperator.h
@@ -79,7 +79,9 @@ public:
     }
 
 private:
+    template<typename K, typename W>
     omnistream::VectorBatch* AggregateWindowOperator<K, W>::createOutputBatch(const std::vector<RowData*>& collectedRows);
+
     void collect(RowKind rowKind, RowData* key, std::unique_ptr<RowData> aggResult) {
         std::vector<RowData*> resultRows;
         reuseOutput->replace(key, aggResult.get());
@@ -103,6 +105,7 @@ private:
 
 };
 
+template<typename K, typename W>
 omnistream::VectorBatch* AggregateWindowOperator<K, W>::createOutputBatch(const std::vector<RowData*>& collectedRows) {
     int numColumns = WindowOperator<K, W>::outputTypes.size();
     const int numRows = static_cast<int>(collectedRows.size());
