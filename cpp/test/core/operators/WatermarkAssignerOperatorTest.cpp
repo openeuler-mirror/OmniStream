afa1babb33e8ed8a777c8ba20ac4fc03c33e8704
Ruiboss <578540600@qq.com>
2026-06-02 22:25:41 +0800

    [fix] Fix some unit tests with memory problems.

--- cpp/test/core/operators/WatermarkAssignerOperatorTest.cpp
+++ cpp/test/core/operators/WatermarkAssignerOperatorTest.cpp
@@ -14,18 +14,19 @@ TEST(WatermarkAssignerOperatorTest, InitTest)
     int timeRowIndex = 0;
     long outOfOrderT = 4000;
     long emissionInterval = 200;
-    OutputTest *out = new OutputTest();
-    WatermarkAssignerOperator *op = new WatermarkAssignerOperator(out, timeRowIndex, outOfOrderT, emissionInterval, new SystemProcessingTimeService());
-    op->open();
+    OutputTest out = OutputTest();
+    auto timerService = SystemProcessingTimeService();
+    WatermarkAssignerOperator op = WatermarkAssignerOperator(&out, timeRowIndex, outOfOrderT, emissionInterval, &timerService);
+    op.open();
 
     int rowCount = 5;
-    omnistream::VectorBatch vb1(rowCount);
+    auto vb1 = new omnistream::VectorBatch(rowCount);
     std::vector<long> vec1 {42, 142, 242, 342, 442};
     std::vector<long> vec2 {41, 42, 43, 44, 45};
-    vb1.Append(omniruntime::TestUtil::CreateVector(rowCount, vec1.data()));
-    vb1.Append(omniruntime::TestUtil::CreateVector(rowCount, vec2.data()));
+    vb1->Append(omniruntime::TestUtil::CreateVector(rowCount, vec1.data()));
+    vb1->Append(omniruntime::TestUtil::CreateVector(rowCount, vec2.data()));
 
-    StreamRecord* record1 = new StreamRecord(&vb1);
-    op->processBatch(record1);
-    op->finish();
+    StreamRecord* record1 = new StreamRecord(vb1);
+    op.processBatch(record1);
+    op.finish();
 }
