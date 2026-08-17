1c1e65a3950f4e17c17e8117a7af18661e61d61c
IcE_test <1054919371@qq.com>
2026-08-17 10:16:51 +0800

    补充ut

--- cpp/test/runtime/checkpoint/AppendOnlyTopNSavepointAdaptorTest.cpp
+++ cpp/test/runtime/checkpoint/AppendOnlyTopNSavepointAdaptorTest.cpp
@@ -148,3 +148,92 @@ TEST(AppendOnlyTopNSavepointAdaptorTest, BatchSizeReturnsFixedValue)
     EXPECT_GT(adaptor.batchSize(0), 0);
     EXPECT_EQ(adaptor.batchSize(0), adaptor.batchSize(1));
 }
+
+// 测试 prepareForSave 正确解析 operatorDescription
+TEST(AppendOnlyTopNSavepointAdaptorTest, PrepareForSaveParsesOperatorDescription)
+{
+    AppendOnlyTopNSavepointAdaptor adaptor;
+    nlohmann::json desc = {{"inputTypes", {"BIGINT", "VARCHAR", "INT"}}, {"sortFieldIndices", {0, 2}}};
+    EXPECT_NO_THROW(adaptor.prepareForSave(desc));
+    auto types = adaptor.columnTypes(0);
+    EXPECT_EQ(types.size(), 3);
+}
+
+// 测试 prepareForRestore 正确解析 operatorDescription
+TEST(AppendOnlyTopNSavepointAdaptorTest, PrepareForRestoreParsesOperatorDescription)
+{
+    AppendOnlyTopNSavepointAdaptor adaptor;
+    nlohmann::json desc = {{"inputTypes", {"BIGINT", "VARCHAR"}}, {"sortFieldIndices", {0}}};
+    EXPECT_NO_THROW(adaptor.prepareForRestore(desc));
+    auto types = adaptor.columnTypes(0);
+    EXPECT_EQ(types.size(), 2);
+}
+
+// 测试 columnTypes 返回与 prepareForRestore 中解析的 inputTypes 一致
+TEST(AppendOnlyTopNSavepointAdaptorTest, ColumnTypesReturnsConsistentTypes)
+{
+    AppendOnlyTopNSavepointAdaptor adaptor;
+    nlohmann::json desc = {{"inputTypes", {"BIGINT", "VARCHAR", "INT"}}, {"sortFieldIndices", {0}}};
+    adaptor.prepareForRestore(desc);
+    auto types = adaptor.columnTypes(0);
+    EXPECT_EQ(types.size(), 3);
+}
+
+// 测试 validateForSave 对空 metaInfos 抛异常
+TEST(AppendOnlyTopNSavepointAdaptorTest, ValidateForSaveRejectsEmptyMetaInfos)
+{
+    AppendOnlyTopNSavepointAdaptor adaptor;
+    std::vector<std::shared_ptr<StateMetaInfoSnapshot>> metas;
+    EXPECT_THROW(adaptor.validateForSave(metas), std::runtime_error);
+}
+
+// 测试 validateForRestore 对空 metaInfos 抛异常
+TEST(AppendOnlyTopNSavepointAdaptorTest, ValidateForRestoreRejectsEmptyMetaInfos)
+{
+    AppendOnlyTopNSavepointAdaptor adaptor;
+    std::vector<std::shared_ptr<StateMetaInfoSnapshot>> metas;
+    EXPECT_THROW(adaptor.validateForRestore(metas), std::runtime_error);
+}
+
+// 测试 buildOmniMainMetaInfo 返回的 metaInfo 包含正确的 serializer
+TEST(AppendOnlyTopNSavepointAdaptorTest, BuildOmniMainMetaInfoContainsSerializers)
+{
+    AppendOnlyTopNSavepointAdaptor adaptor;
+    auto flinkMeta = makeKvMap("data-state-with-append");
+    auto omniMeta = adaptor.buildOmniMainMetaInfo(0, *flinkMeta);
+    auto* nsSerializer = omniMeta.getTypeSerializer("NAMESPACE_SERIALIZER");
+    EXPECT_NE(nsSerializer, nullptr);
+}
+
+// 测试多个 adaptor 实例互不影响
+TEST(AppendOnlyTopNSavepointAdaptorTest, MultipleAdaptorsAreIndependent)
+{
+    AppendOnlyTopNSavepointAdaptor adaptor1;
+    AppendOnlyTopNSavepointAdaptor adaptor2;
+
+    nlohmann::json desc1 = {{"inputTypes", {"BIGINT"}}, {"sortFieldIndices", {0}}};
+    nlohmann::json desc2 = {{"inputTypes", {"VARCHAR", "INT"}}, {"sortFieldIndices", {1}}};
+
+    adaptor1.prepareForSave(desc1);
+    adaptor2.prepareForSave(desc2);
+
+    EXPECT_EQ(adaptor1.columnTypes(0).size(), 1);
+    EXPECT_EQ(adaptor2.columnTypes(0).size(), 2);
+}
+
+// 测试 validateForSave 对多个状态只检查第一个
+TEST(AppendOnlyTopNSavepointAdaptorTest, ValidateForSaveChecksFirstState)
+{
+    AppendOnlyTopNSavepointAdaptor adaptor;
+    std::vector<std::shared_ptr<StateMetaInfoSnapshot>> metas{makeKv("data-state-with-append"), makeKv("other-state")};
+    EXPECT_NO_THROW(adaptor.validateForSave(metas));
+}
+
+// 测试 validateForRestore 对多个状态只检查第一个
+TEST(AppendOnlyTopNSavepointAdaptorTest, ValidateForRestoreChecksFirstState)
+{
+    AppendOnlyTopNSavepointAdaptor adaptor;
+    std::vector<std::shared_ptr<StateMetaInfoSnapshot>> metas{
+        makeKvMap("data-state-with-append"), makeKvMap("other-state")};
+    EXPECT_NO_THROW(adaptor.validateForRestore(metas));
+}
